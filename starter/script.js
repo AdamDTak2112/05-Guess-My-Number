e56c0f2c365553b2ff3359ef589f0b40db982e5e
Adam Tak <adam.tak2112@gmail.com>
2021-01-25 13:35:17 -0700

    implementing basic game logic

--- starter/script.js
+++ starter/script.js
@@ -8,8 +8,27 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNum;
+
 document.querySelector('.check').addEventListener('click',
     function () {
-        console.log(document.querySelector('.guess').value)
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(guess, typeof guess);
+
+        if (!guess) {
+            document.querySelector('.message').textContent = 'No number!'
+        } else if (guess === secretNum) {
+            document.querySelector('.message').textContent = 'Correct number!';
+        } else if (guess > secretNum) {
+            score--;
+            document.querySelector('.message').textContent = 'Too high!';
+            document.querySelector('.score').textContent = score;
+        } else {
+            score--;
+            document.querySelector('.message').textContent = 'Too low!';
+            document.querySelector('.score').textContent = score;
+        }
     }
 );
